414b9da90423cbbc70ce7f0aeee5eee2d40c98fe
Kev Ellis <kev@ellis-online.co.uk>
2015-08-01 20:17:26 +0100

    Add MIT licence text

--- js/slotmachine.js
+++ js/slotmachine.js
@@ -1,6 +1,47 @@
 /*jslint browser: true*/
 /*global THREE, Stats*/
 
+/*
+---------------------------------------------------------------------------
+
+VIRTUAL SLOT MACHINE
+====================
+
+Written By Kevin Ellis August 2015.
+
+A simple 3D "Slot Machine" game.
+This game uses the excellent three.js WebGL library and was mainly written
+as a learning exercise for three.js and JavaScript techniques. As a result
+my code may not be the most robust or eloquent, but it might be of some use
+to others starting out with 3D browser programming.
+
+---------------------------------------------------------------------------
+
+The MIT License (MIT)
+
+Copyright (c) 2015 Kevin Ellis
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+---------------------------------------------------------------------------
+*/
+
 function virtualSlotMachine() {
   'use strict';
 
