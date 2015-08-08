ea6c9b6fcf4c07ef0581331cf8e1da3224fe5418
Kev Ellis <kev@ellis-online.co.uk>
2015-08-08 13:08:45 +0100

    Add build script

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Linux Bash script to build and update the gh-pages branch based on changes made in the master branch.
+# Only the commit and push are left to be done manually.
+
+git checkout master;
+npm install;
+grunt;
+git checkout gh-pages;
+cd gh-pages;
+cp -R * ..;
+git add --all .;
+cd ..;
+git add --all .;
+
+echo "Now just check git status and if all green commit and push!"
+#git commit -m 'update to latest build'
+#git push
